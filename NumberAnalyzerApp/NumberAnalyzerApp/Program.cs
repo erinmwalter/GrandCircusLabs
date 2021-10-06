3029d0d37f561a189fcc2e760405516a21b200eb
Erin Walter <ewalter@rocketmortgage.com>
2021-10-06 14:15:56 -0400

    Adding First Two Labs

---
+++ NumberAnalyzerApp/NumberAnalyzerApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace NumberAnalyzerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            string userName;
+            char runAgain = 'y';
+
+            //welcome user to app and get user's name and number input
+            Console.WriteLine("Welcome To the Number Analyzer App!");
+            Console.Write("What is your name? ");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Welcome, {userName}!");
+
+            //prompts user to enter a number between 1 and 100
+            while (runAgain == 'y')
+            {
+                Console.Write("Enter an Integer between 1 and 100: ");
+                int.TryParse(Console.ReadLine(), out userNumber);
+
+                //input validation, will continue to prompt user to enter a different number
+                //if number not between 1 and 100
+                while (userNumber < 1 || userNumber > 100)
+                {
+                    Console.WriteLine($"I'm sorry, {userName}, {userNumber} is not a valid input.");
+                    Console.Write("Please enter an integer between 1 and 100: ");
+                    userNumber = int.Parse(Console.ReadLine());
+                }
+
+                //sends number to be analyzed
+                AnalyzeNumber(userNumber);
+
+                //asks user if they would like to run again
+                Console.Write($"{userName}, would you like to enter another number? y/n: ");
+                runAgain = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nProgram Exited. Goodbye!");
+
+
+
+
+        }
+
+        //this method will analyze the number through a series of if/else statements
+        //and generate output to console depending on the number's value
+        public static void AnalyzeNumber(int number)
+        {
+            if(number % 2 == 0 && (number >= 2 && number <= 25))
+            {
+                Console.WriteLine("Even and less than 25.");
+            }
+            else if (number % 2 == 0 && (number >= 26 && number <= 60))
+            {
+                Console.WriteLine("Even.");
+            }
+            else if (number % 2 == 0 && number > 60)
+            {
+                Console.WriteLine($"{number}. Even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number}. Odd.");
+            }
+
+            //Lab Summary Question: this "ELSE" statement above covers all odd numbers.
+            //the first condition was if the number is ODD at all it prints the number and odd.
+            //the last condition said if the number > 60 and ODD it prints the number and odd.
+            //since these can be covered in one case and because even numbers 1-100 were accounted
+            //for in all other cases, I threw this into the "else" portion instead of making additional if else
+            //statements for these. 
+        }
+    }
+}
---
+++ RoomCalculatorApp/RoomCalculatorApp/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace RoomCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomHeight, roomLength, roomWidth, floorArea, roomVolume, wallArea, roomPerimeter;
+            int numCarpetSquares, gallonsPaint;
+            char runAgain = 'y';
+
+
+            Console.WriteLine("Welcome to the Room Calculator App.");
+
+            while (runAgain == 'y')
+            {
+                //get user input for length, height, width of room
+                roomLength = GetRoomLength();
+                roomWidth = GetRoomWidth();
+                roomHeight = GetRoomHeight();
+
+                //calculate room dimensions
+                floorArea = GetFloorArea(roomLength, roomWidth);
+                roomVolume = GetRoomVolume(roomLength, roomWidth, roomHeight);
+                roomPerimeter = GetRoomPerimeter(roomLength, roomWidth);
+                wallArea = GetWallArea(roomPerimeter, roomHeight);
+
+                //calculate amount of carpet and paint needed
+                numCarpetSquares = GetCarpetAmount(floorArea);
+                gallonsPaint = GetGallonsPaint(wallArea);
+
+                //display all values to user
+                Console.WriteLine();
+                Console.WriteLine("You Entered: ");
+                Console.WriteLine($"Room Length: {roomLength}, Room Width: {roomWidth}, Room Height: {roomHeight}");
+                Console.WriteLine();
+                Console.WriteLine($"Room Area: {floorArea}");
+                Console.WriteLine($"Room Perimeter: {roomPerimeter}");
+                Console.WriteLine($"Room Volume: {roomVolume}");
+                Console.WriteLine();
+                Console.WriteLine($"You need {numCarpetSquares} carpet squares and {gallonsPaint} gallons paint.");
+                Console.WriteLine();
+
+                //prompt user if they want to run program again
+                Console.WriteLine("Would you like to enter a different room (y/n)?");
+                runAgain = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Program Exited. Goodbye.");
+
+
+        }
+
+        //method to have user enter room length
+        public static double GetRoomLength()
+        {
+            double roomLength;
+
+            Console.Write("Enter Length of Room: ");
+            string userInput = Console.ReadLine();
+            roomLength = double.Parse(userInput);
+
+            return roomLength;
+        }
+
+        //method to have user enter room width
+        public static double GetRoomWidth()
+        {
+            double roomWidth;
+
+            Console.Write("Enter Width of Room: ");
+            string userInput = Console.ReadLine();
+            roomWidth = double.Parse(userInput);
+
+            return roomWidth;
+        }
+
+        //method to have user enter height of room
+        public static double GetRoomHeight()
+        {
+            double roomHeight;
+
+            Console.Write("Enter Height of Room: ");
+            string userInput = Console.ReadLine();
+            roomHeight = double.Parse(userInput);
+
+            return roomHeight;
+        }
+       
+
+        //method calculates the area of the floor 
+        public static double GetFloorArea(double roomWidth, double roomLength)
+        {
+            return (roomWidth * roomLength);
+
+        }
+
+        //method calculates the area of the walls
+        public static double GetWallArea(double roomPerimeter, double roomHeight)
+        {
+            return (roomPerimeter * roomHeight);
+
+        }
+
+        //method calculates the volume of the room
+        public static double GetRoomVolume(double roomWidth, double roomLength, double roomHeight)
+        {
+            return roomHeight * roomWidth * roomLength;
+
+        }
+
+        //method calculates the perimeter of the room
+        public static double GetRoomPerimeter(double roomWidth, double roomLength)
+        {
+            return ((2 * roomWidth) + (2 * roomLength));
+
+        }
+        //method calculates number of carpet tiles needed (rounded up to nearest integer value)
+        public static int GetCarpetAmount(double floorArea)
+        {
+            return (int)Math.Ceiling(floorArea / 5);
+        }
+
+        //method calculates how many gallons of pain needed for walls (rounded up to nearest integer value)
+        public static int GetGallonsPaint(double wallArea)
+        {
+            return (int)Math.Ceiling(wallArea / 5);
+        }
+    }
+}
