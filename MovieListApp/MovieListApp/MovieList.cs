e7424cd2988d0050a7f955b69e2106dd3019822b
Erin Walter <erinwalter@rocketmortgage.com>
2021-10-18 16:55:28 -0400

    Movie List Lab Added

---
+++ MovieListApp/MovieListApp/MovieList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieListApp
+{
+    class MovieList
+    {
+        public List<Movie> movies { get; set; } = new List<Movie>();
+
+        //constructor, adds 10 movies to list when movielist initialized
+        public MovieList()
+        {
+            movies.Add(new Movie("Toy Story", "Animated"));
+            movies.Add(new Movie("Star Wars", "Sci-Fi"));
+            movies.Add(new Movie("The Conjuring", "Horror"));
+            movies.Add(new Movie("Aladdin", "Animated"));
+            movies.Add(new Movie("Halloween", "Horror"));
+            movies.Add(new Movie("Black Panther", "Drama"));
+            movies.Add(new Movie("Mission Impossible", "Drama"));
+            movies.Add(new Movie("The Ring", "Horror"));
+            movies.Add(new Movie("Alien", "Sci-Fi"));
+            movies.Add(new Movie("E.T.", "Sci-Fi"));
+            movies.Add(new Movie("Lilo and Stitch", "Animated"));
+            movies.Add(new Movie("Silver Linings Playbook", "Drama"));
+
+        }
+
+        //method to display the list in alphabetical order
+        public void DisplayAlphabeticalList()
+        {
+            List<Movie> alphabeticalMovies = movies.OrderBy(x => x.Name).ToList();
+
+            Console.WriteLine("Current Movie List:");
+            for (int i = 0; i < alphabeticalMovies.Count; i++)
+            {
+                Console.WriteLine($"{alphabeticalMovies[i].Name}, Genre: {alphabeticalMovies[i].Genre}");
+            }
+        }
+
+        //returns number of movies in list
+        public int GetMovieCount()
+        {
+            return movies.Count;
+        }
+
+        //gets user input
+        public string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+
+        //method displays by genre after prompting user for a specific genre. 
+        public void DisplayByGenre()
+        {
+            List<string> genreList = new List<string>();
+            bool goodInput = false;
+            int genre = 0;
+            //this loop looks through all genres and adds them to the menu for user
+            //to choose from.
+            foreach (Movie movie in movies)
+            {
+                if (!genreList.Contains(movie.Genre))
+                {
+                    genreList.Add(movie.Genre);
+                }
+            }
+            //writes out menu based on the list that was just created of genre options.
+            Console.WriteLine("Genre Menu:");
+            for (int i = 0; i < genreList.Count; i++)
+            {
+                Console.WriteLine($"{i}: {genreList[i]}");
+            }
+
+            //added try catch to validate input here
+            while (!goodInput)
+            {
+                string input = GetInput("Enter selection: ");
+                try
+                {
+                    genre = int.Parse(input);
+                    Console.WriteLine($"{genreList[genre]} Movies:");
+                    foreach (Movie movie in movies)
+                    {
+                        if (genreList[genre] == movie.Genre)
+                        {
+                            Console.WriteLine(movie.Name);
+                        }
+                    }
+                    break;
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input, must be integer. try again. ");
+                    continue;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Invalid meu selection, must be between 0 and {genreList.Count-1}");
+                    continue;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ MovieListApp/MovieListApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MovieListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieList myMovies = new MovieList();
+            bool runAgain = true;
+
+            Console.WriteLine("Welcome to the Movie List App.");
+            Console.WriteLine($"There are {myMovies.GetMovieCount()} movies on this list.\n");
+
+            while (runAgain)
+            {
+                //main menu
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. View movies by Genre.");
+                Console.WriteLine("2. View Entire List.");
+                Console.WriteLine("3. Exit.");
+
+                string userChoice = GetInput("What would you like to do: ");
+                if (userChoice == "1")
+                {
+                    //asks user for genre and displays by genre
+                    myMovies.DisplayByGenre();
+                }
+                else if (userChoice == "2")
+                {
+                    //displays entire alphabetized list
+                    myMovies.DisplayAlphabeticalList();
+                }
+                else if (userChoice == "3")
+                {
+                    //program exit so breaks from loop
+                    break;
+                }
+                else
+                {
+                    //error catching.
+                    Console.WriteLine("Invalid Input, try again.");
+                    continue;
+                }
+
+                runAgain = Continue();
+            }
+            Console.WriteLine("Goodbye");
+        }
+
+        //asks user if they would like to run program again
+        public static bool Continue()
+        {
+            Console.WriteLine("Would you like to go again? (y/n): ");
+            string answer = Console.ReadLine().Trim().ToLower();
+
+            if(answer == "y")
+            {
+                return true;
+            }
+            else if (answer == "n")
+            {
+                return false;
+            }
+            else
+            {
+                return Continue();
+            }
+        }
+
+        //gets user input
+        public static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
