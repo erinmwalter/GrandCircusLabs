f14ceda920c5b1133831f7f277252230ca2ce4aa
Erin Walter <erinwalter@rocketmortgage.com>
2021-10-20 09:23:48 -0400

    Re-uploading just to make sure Circle App updates were pushed

---
+++ CircleAPp/CircleAPp/CircleList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleApp
+{
+    class CircleList
+    {
+        public List<Circle> circleList = new List<Circle>();
+
+        //method to display the circle list to the screen
+        public void DisplayCircleList()
+        {
+            Console.WriteLine($"You Entered {circleList.Count} circles.");
+            Console.WriteLine($"Here they are:\n");
+            for (int i = 0; i < circleList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}: {circleList[i]}");
+            }
+        }
+
+        //method to add circle to list
+        public void AddCircle(Circle circle)
+        {
+            circleList.Add(circle);
+        }
+
+        //this method validates the user input to ensure it's a valid positive double value
+        public double GetValidInput(string prompt)
+        {
+            bool goodInput = false;
+            double radius = 0;
+            while (!goodInput)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    radius = double.Parse(Console.ReadLine());
+                    if (radius <= 0)
+                    {
+                        throw new InvalidNegativeEntry();
+                    }
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input, must be a double-type value.");
+                    continue;
+                }
+                catch (InvalidNegativeEntry e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+            return radius;
+
+
+        }
+
+        //custom exception to throw if input invalid. 
+        public class InvalidNegativeEntry : Exception
+        {
+            public override string Message
+            {
+                get
+                {
+                    return "Radius Must be > 0. Try Again.";
+                }
+            }
+        }
+    }
+}
--- CircleAPp/CircleAPp/Program.cs
+++ CircleAPp/CircleAPp/Program.cs
@@ -7,7 +7,7 @@ namespace CircleApp
     {
         static void Main(string[] args)
         {
-            List<Circle> circleList = new List<Circle>();
+            CircleList circleList = new CircleList();
             bool runAgain = true;
 
             Console.WriteLine("Welcome to the Circle App.");
@@ -15,20 +15,15 @@ namespace CircleApp
             //loops and adds circles until the user wants to stop entering
             while (runAgain) 
             {
-                double radius = GetValidInput("Enter Circle Radius: ");
+                double radius = circleList.GetValidInput("Enter Circle Radius: ");
                 Circle circle = new Circle(radius);
-                circleList.Add(circle);
+                circleList.AddCircle(circle);
                 runAgain = Continue("Enter Another Circle? (y/n): ");
             }
 
             //once user is done entering circles this will display each circle info in list.
             Console.Clear();
-            Console.WriteLine($"You Entered {circleList.Count} circles.");
-            Console.WriteLine($"Here they are:\n");
-            for (int i = 0; i < circleList.Count; i++)
-            {
-                Console.WriteLine($"{i+1}: {circleList[i]}");
-            }
+            circleList.DisplayCircleList();
             Console.WriteLine("\nGoodbye!");
 
         }
@@ -36,7 +31,7 @@ namespace CircleApp
         //continue method that prompts user if they want to go on
         public static bool Continue(string prompt)
         {
-            Console.WriteLine(prompt);
+            Console.Write(prompt);
             string input = Console.ReadLine().Trim().ToLower();
             if (input == "y")
             {
@@ -53,50 +48,7 @@ namespace CircleApp
             }
         }
 
-        //this method validates the user input to ensure it's a valid positive double value
-        public static double GetValidInput(string prompt)
-        {
-            bool goodInput = false;
-            double radius = 0;
-            while (!goodInput)
-            {
-                try
-                {
-                    Console.Write(prompt);
-                    radius = double.Parse(Console.ReadLine());
-                    if (radius <= 0)
-                    {
-                        throw new InvalidNegativeEntry();
-                    }
-                    break;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid input, must be a double-type value.");
-                    continue;
-                }
-                catch (InvalidNegativeEntry e)
-                {
-                    Console.WriteLine(e.Message);
-                    continue;
-                }
-            }
-            return radius;
-            
 
-        }
-
-        //custom exception to throw if input invalid. 
-        public class InvalidNegativeEntry : Exception
-        {
-            public override string Message
-            {
-                get
-                {
-                    return "Radius Must be > 0. Try Again.";
-                }
-            }
-        }
 
     }
 }
